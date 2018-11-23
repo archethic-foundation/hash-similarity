c3464020a28ef6fa36251d8cffda8d53286a4b76
Samuel Manzanera <samuel@uniris.io>
2018-11-23 16:35:48 +0100

    Init repo

---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	crand "crypto/rand"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"flag"
+	"io"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/AllenDang/simhash"
+	"github.com/dexyk/stringosim"
+)
+
+func main() {
+
+	t := flag.Float64("t", 0.5, "Threshold similarity to reach before stoping the address generation")
+	lev := flag.Bool("lev", false, "Leveinshtein algorithm")
+	ham := flag.Bool("ham", false, "Hamming algorithm")
+	sim := flag.Bool("sim", false, "Simhash algorithm")
+	jaro := flag.Bool("jar", false, "Jaro Winkler algorithm")
+	cos := flag.Bool("cos", false, "Cosine algorithm")
+	lcs := flag.Bool("lcs", false, "Longest Common Subsequence algorithm")
+	flag.Parse()
+
+	if *lev == false && *ham == false && *sim == false && *jaro == false && *cos == false && *lcs == false {
+		flag.Usage()
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	dataHash := "843a54527fdcedfabc0e8f0234e76f63c1d149d26790531998c43d73e455e0da"
+
+	var r float64
+
+	cycles := 0
+	var key string
+	for r < *t {
+		key = randomKeyHash()
+		if *lev {
+			dist := stringosim.Levenshtein([]rune(dataHash), []rune(key))
+			r = 1 - float64(dist)/64
+			log.Print(r)
+		}
+
+		if *sim {
+			r = simhash.GetLikenessValue(dataHash, key)
+			log.Print(r)
+		}
+
+		if *ham {
+			dist, _ := stringosim.Hamming([]rune(dataHash), []rune(key))
+			r = 1 - float64(dist)/64
+			log.Print(r)
+		}
+
+		if *jaro {
+			r = stringosim.JaroWinkler([]rune(dataHash), []rune(key))
+			log.Print(r)
+		}
+
+		if *cos {
+			r = stringosim.Cosine([]rune(dataHash), []rune(key))
+			log.Print(r)
+		}
+
+		if *lcs {
+			dist := stringosim.LCS([]rune(dataHash), []rune(key))
+			r = 1 - (float64(dist) / 64)
+			log.Print(r)
+		}
+
+		cycles++
+	}
+
+	log.Printf("data hash to compared: %s\n", dataHash)
+	log.Printf("hash of the generated key similar: %s\n", key)
+	log.Printf("%2.f percent similarity after %d keys generated", *t*100, cycles)
+}
+
+func randomKeyHash() string {
+	key, _ := ecdsa.GenerateKey(elliptic.P256(), crand.Reader)
+	pubKey, _ := x509.MarshalPKIXPublicKey(key.Public())
+
+	hash := sha256.New()
+	hash.Write(pubKey)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func randomHash() string {
+	hash := sha256.New
+
+	r := make([]byte, hash().Size())
+	io.ReadFull(crand.Reader, r)
+	return hex.EncodeToString(r)
+}
